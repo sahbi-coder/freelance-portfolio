977c064547ed5a5858d8d092ec569c2d1a7ed57a
unknown <you@example.com>
2023-01-04 01:31:59 +0100

    last fix

--- pages/galery.js
+++ pages/galery.js
@@ -3,22 +3,40 @@ import axios from "axios";
 import downloadImages from "../functions/downloadImagesFromFirebase";
 import Image from "next/image";
 import Modal from "../components/Modal";
-import { useRef } from "react";
+import { useRef, useEffect, useState } from "react";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 import Layout from "../components/Layout";
 import Head from "next/head";
 
-export default function Galery({ images }) {
+export default function Galery() {
   const modalRef = useRef(null);
+  const [images, setImages] = useState([]);
   const openModal = (img) => {
     modalRef.current.openModal(img);
   };
   const { t } = useTranslation();
+  useEffect(() => {
+    (async function getImages() {
+      try {
+        let res = [];
+        let result = (
+          await axios.get(process.env.NEXT_PUBLIC_BASE_URL + "/api/images")
+        ).data;
+        for (let i = 0; i < result.length; i++) {
+          let src = await downloadImages(result[i].img);
+          let image = { id: result[i].id, src };
+          res.push(image);
+        }
+      } catch {
+        res = [];
+      }
+      setImages(res);
+    })();
+  }, []);
   return (
     <>
       <Head>
-        
         <meta name="description" content="Oussama jedda | Website" />
         <link rel="icon" href="/icon.svg" />
         <title>oussama jedda | galery</title>
@@ -46,29 +64,10 @@ export default function Galery({ images }) {
   );
 }
 
-export async function getServerSideProps({locale}) {
-  async function getImages() {
-    try {
-      let res = [];
-      let result = (
-        await axios.get(process.env.NEXT_PUBLIC_BASE_URL + "/api/images")
-      ).data;
-      for (let i = 0; i < result.length; i++) {
-        let src = await downloadImages(result[i].img);
-        let image = { id: result[i].id, src };
-        res.push(image);
-      }
-
-      return res;
-    } catch {
-      return [];
-    }
-  }
-  const images = await getImages();
+export async function getStaticProps({ locale }) {
   return {
     props: {
       ...(await serverSideTranslations(locale, ["common", "galery", "navbar"])),
-      images,
     },
   };
 }
--- pages/index.js
+++ pages/index.js
@@ -9,38 +9,49 @@ import { serverSideTranslations } from "next-i18next/serverSideTranslations";
 import { useTranslation } from "next-i18next";
 import downloadImages from "../functions/downloadImagesFromFirebase";
 import axios from "axios";
+import { useEffect, useState } from "react";
 
-export async function getServerSideProps({locale}){
-  async function getImages() {
-    try {
-      let res = [];
-      let result = (await axios.get(process.env.NEXT_PUBLIC_BASE_URL+"/api/images")).data;
-      for (let i = 0; i < result.length; i++) {
-        let src = await downloadImages(result[i].img);
-        let image = { id: result[i].id, src };
-        res.push(image);
-      }
-      return res
-    } catch {
-      return []
-    }
-  }
-  const images = await getImages()
 
+export async function getStaticProps({ locale }) {
   return {
-    props:{
-      ...(await serverSideTranslations(locale,['common','canvas','about','services','work','contact','navbar'])),images
-    }
-  }
+    props: {
+      ...(await serverSideTranslations(locale, [
+        "common",
+        "canvas",
+        "about",
+        "services",
+        "work",
+        "contact",
+        "navbar",
+      ])),
+    },
+  };
 }
 
-
-export default function Home({images}) {
-  const {t}=useTranslation()
+export default function Home() {
+  const { t } = useTranslation();
+  const [images, setImages] = useState([]);
+  useEffect(() => {
+    (async function getImages() {
+      try {
+        let res = [];
+        let result = (
+          await axios.get(process.env.NEXT_PUBLIC_BASE_URL + "/api/images")
+        ).data;
+        for (let i = 0; i < result.length; i++) {
+          let src = await downloadImages(result[i].img);
+          let image = { id: result[i].id, src };
+          res.push(image);
+        }
+      } catch {
+        res = [];
+      }
+      setImages(res);
+    })();
+  }, []);
   return (
     <>
       <Head>
-        
         <meta name="description" content="Oussama jedda | Website" />
         <link rel="icon" href="/icon.svg" />
         <title>oussama jedda</title>
@@ -48,13 +59,13 @@ export default function Home({images}) {
 
       <Layout t={t}>
         <main>
-          <Canvas t={t}/>
-          <About t={t}/>
-          <Services t={t}/>
-          <Work t={t} images={images}/>
+          <Canvas t={t} />
+          <About t={t} />
+          <Services t={t} />
+          <Work t={t} images={images} />
         </main>
         <footer>
-          <Contact t={t}/>
+          <Contact t={t} />
         </footer>
       </Layout>
     </>
